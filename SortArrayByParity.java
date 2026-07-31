fb93c8611b665ae30f8618bbdff1c5437558d4db
varrssshhhhh <vtu28801@veltech.edu.in>
2026-07-31 10:13:48 +0530

    Refactor SortArrayByParity to Solution class
    Removed the main method and adjusted class declaration.

--- SortArrayByParity.java
+++ SortArrayByParity.java
@@ -1,4 +1,4 @@
-public class SortArrayByParity {
+class Solution {
 
     public int[] sortArrayByParity(int[] nums) {
         int left = 0;
@@ -11,21 +11,13 @@ public class SortArrayByParity {
                 nums[right] = temp;
             }
 
-            if (nums[left] % 2 == 0) left++;
-            if (nums[right] % 2 == 1) right--;
+            if (nums[left] % 2 == 0)
+                left++;
+
+            if (nums[right] % 2 == 1)
+                right--;
         }
 
         return nums;
-    }
-
-    public static void main(String[] args) {
-        int[] nums = {3, 1, 2, 4};
-
-        SortArrayByParity obj = new SortArrayByParity();
-        int[] result = obj.sortArrayByParity(nums);
-
-        for (int num : result) {
-            System.out.print(num + " ");
-        }
     }
 }
